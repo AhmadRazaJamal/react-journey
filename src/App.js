47875fc9737679a93981f78e674279b24834e809
AhmadRazaJamal <ahmad.raza@me.com>
2020-10-13 12:27:41 -0500

    Tested some JS objects being passed as vars in JSX

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 
 const name = "Ahmad Jamal";
 
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
