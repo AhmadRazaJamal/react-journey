e65921c15731021e7a210ec4b8fb4f20d7e16712
AhmadRazaJamal <ahmad.raza@me.com>
2020-10-13 12:23:08 -0500

    Initialized the react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const name = "Ahmad Jamal";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello {name}</h1>
+    </div>
+  );
+}
+
+export default App;
