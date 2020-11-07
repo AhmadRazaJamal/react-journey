3868cdd823ebcb5a86b8d1ebb34b14f97cffc771
AhmadRazaJamal <ahmad.raza@me.com>
2020-11-06 18:12:07 -0600

    Added Material UI buttons, made a couple of CSS changes and updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import axios from 'axios';
 import React from 'react';
+import { sortBy } from 'lodash';
+import SearchIcon from '@material-ui/icons/Search';
+import { TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 import './App.css';
 import { ReactComponent as Check } from './check.svg';
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    width: '80%',
+    fontfamily: 'Lato',
+    fontSize:"18px",
+  },
+  searchButton: {
+    background: 'white',
+    color: 'black',
+    fontSize: '18px',
+    fontFamily: 'Lato',
+    '&:hover': {
+        color: 'white',
+        background: 'black',
+    }
+  },
+  filterButton: {
+    color: 'black',
+    fontFamily: 'Lato',
+    '&:hover': {
+      color: 'white',
+      background: 'black',
+      border: '1px white solid'
+  }
+  }
+}));
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -16,6 +48,14 @@ const useSemiPersistentState = (key, initialState) => {
   }, [value, key]);
 
   return [value, setValue];
+};
+
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'author'),
+  COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+  POINTS: list => sortBy(list, 'points').reverse(),
 };
 
 const storiesReducer = (state, action) => {
@@ -104,7 +144,7 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1 className="headline-primary">My Hacker Stories</h1>
+      <h1 className="headline-primary">My React Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -127,7 +167,10 @@ const SearchForm = ({
   searchTerm,
   onSearchInput,
   onSearchSubmit,
-}) => (
+}) => {
+  const classes = useStyles();
+
+  return(
   <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
@@ -135,18 +178,22 @@ const SearchForm = ({
       isFocused
       onInputChange={onSearchInput}
     >
-      Search:
     </InputWithLabel>
 
-    <button
+    <Button
       type="submit"
+      variant="contained"
       disabled={!searchTerm}
-      className="button button_large"
+      className={classes.searchButton}
+      style={{marginLeft: '1%',padding:'1% 4%', width: '20%',}}
+      size="large"
+      startIcon={<SearchIcon/>}
     >
       Submit
-    </button>
+    </Button>
   </form>
-);
+  );
+};
 
 const InputWithLabel = ({
   id,
@@ -163,6 +210,7 @@ const InputWithLabel = ({
       inputRef.current.focus();
     }
   }, [isFocused]);
+  const classes = useStyles();
 
   return (
     <>
@@ -170,43 +218,73 @@ const InputWithLabel = ({
         {children}
       </label>
       &nbsp;
-      <input
+      <TextField
         ref={inputRef}
         id={id}
+        color={"primary"}
         type={type}
         value={value}
         onChange={onInputChange}
-        className="input"
+        className={classes.input}
+        variant="outlined" 
+        label="Search Term"
       />
     </>
   );
 };
 
-const List = ({ list, onRemoveItem }) =>
-  list.map(item => (
+const List = ({ list, onRemoveItem }) => {
+  
+  const [sort, setSort] = React.useState({ sortKey: "NONE", isReverse: false });
+ 
+  const sortFunction = SORTS[sort.sortKey];
+  const sortedList = sort.isReverse ? sortFunction(list) : sortFunction(list).reverse();
+
+  const classes = useStyles();
+
+  const handleSort = sortKey => {
+    const isReverse = sort.sortKey === sortKey && !sort.isReverse ;
+    setSort({sortKey, isReverse})
+  }
+
+  return(
+  <div>
+    <div className="list-header" style={{ display: 'flex'}}>
+    <span style={{ width: '44%' }}><Button variant="outlined" className={classes.filterButton} onClick={() => handleSort("TITLE")}>Title</Button></span>
+    <span style={{ width: '30%' }}><Button variant="outlined" className={classes.filterButton} onClick={() => handleSort("AUTHOR")}>Author</Button></span>
+    <span style={{ width: '16%' }}><Button variant="outlined" className={classes.filterButton} onClick={() => handleSort("COMMENTS")}>Comments</Button></span>
+    <span style={{ width: '13%' }}><Button variant="outlined" className={classes.filterButton} onClick={() => handleSort("POINTS")}>Points</Button></span>
+    <span style={{ width: '10%', marginTop: '0.6%' }}>ACTIONS</span>
+    </div>
+  {sortedList.map(item => (
     <Item
       key={item.objectID}
       item={item}
       onRemoveItem={onRemoveItem}
     />
-  ));
+  ))}
+</div>
+  );
+}
 
 const Item = ({ item, onRemoveItem }) => (
-  <div className="item">
-    <span style={{ width: '40%' }}>
+  <div className="item" style={{display: "flex"}}>
+    <span style={{ width: '40%', marginLeft: '0.8%',  }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '30%', marginLeft: '1.2%', }}>{item.author}</span>
     <span style={{ width: '10%' }}>{item.num_comments}</span>
-    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%', marginLeft: '2.2%' }}>{item.points}</span>
     <span style={{ width: '10%' }}>
-      <button
-        type="button"
+      <Button
+        type="Button"
         onClick={() => onRemoveItem(item)}
-        className="button button_small"
+        variant="contained"
+        color="secondary"
+        startIcon={<CancelIcon/>}
       >
-        <Check height="18px" width="18px" />
-      </button>
+        Delete
+      </Button>
     </span>
   </div>
 );
