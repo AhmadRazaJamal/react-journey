cb87d764f11cb50621f3cf41d8f440b02d378834
AhmadRazaJamal <ahmad.raza@me.com>
2020-12-26 17:33:33 -0600

    Updates

---
+++ precache-manifest.91371720e06fea801bccfaab2087de6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca89838dd73d8c788baf0f05200d4afb",
+    "url": "/react-journey/index.html"
+  },
+  {
+    "revision": "249e24848832316aaee5",
+    "url": "/react-journey/static/css/main.47da64d0.chunk.css"
+  },
+  {
+    "revision": "754c7f437e6fdc1926f9",
+    "url": "/react-journey/static/js/2.7f0e08c4.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/react-journey/static/js/2.7f0e08c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249e24848832316aaee5",
+    "url": "/react-journey/static/js/main.278b4449.chunk.js"
+  },
+  {
+    "revision": "6e4dccf2d1dcac538fc4",
+    "url": "/react-journey/static/js/runtime-main.955d4f9b.js"
+  },
+  {
+    "revision": "973c16f233137798904ff812348c6c88",
+    "url": "/react-journey/static/media/check.973c16f2.svg"
+  }
+]);
